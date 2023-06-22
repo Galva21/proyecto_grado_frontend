f7384030b896530b160d5a330d8357a77217f93b
Alvaro Gonzales <alvarojuan2014@gmail.com>
2023-06-22 12:14:13 -0400

    Conexion flutterfire con la app

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ import 'package:pet_app/screens/splash_screen.dart';
 import 'package:provider/provider.dart';
 import 'screens/voluntario/root_voluntario.dart';
 import 'utils/color.dart';
-void main() {
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
